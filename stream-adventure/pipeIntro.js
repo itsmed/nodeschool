00fa47cc866df4f1eef2c0764b073da692cec665
DejonGill <dejonjgill@gmail.com>
2016-11-06 20:49:17 -0800

    pipe!

---
+++ stream-adventure/pipeIntro.js
@@ -0,0 +1,6 @@
+'use strict';
+(function () {
+    var fs = require('fs');
+    var filePath = process.argv[2];
+    fs.createReadStream(filePath).pipe(process.stdout);
+}());
---
+++ stream-adventure/pipeIntro.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+(function(){
+  const fs = require('fs');
+  const filePath = process.argv[2];
+
+  fs.createReadStream(filePath).pipe(process.stdout);
+}());
