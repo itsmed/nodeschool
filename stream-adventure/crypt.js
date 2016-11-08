2fda2847b520c0f0d8bf2466809ee1b84a63d8f9
DejonGill <dejonjgill@gmail.com>
2016-11-08 11:19:05 -0800

    I want to play with crypto more

---
+++ stream-adventure/crypt.js
@@ -0,0 +1,6 @@
+'use strict';
+(function () {
+    var crypto = require('crypto');
+    var stream = crypto.createDecipher('aes256', process.argv[2]);
+    process.stdin.pipe(stream).pipe(process.stdout);
+}());
---
+++ stream-adventure/crypt.ts
@@ -0,0 +1,9 @@
+'use strict';
+(function() {
+
+  const crypto = require('crypto');
+  let stream = crypto.createDecipher('aes256', process.argv[2]);
+
+  process.stdin.pipe(stream).pipe(process.stdout);
+
+}());
