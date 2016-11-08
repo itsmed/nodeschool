5b25bb07d33955c7c18ab7291c01a2bc96769157
DejonGill <dejonjgill@gmail.com>
2016-11-08 13:51:39 -0800

    I need an idea for an electron project

---
+++ elementartyElectron/app.ts
@@ -0,0 +1,9 @@
+const electron = require('electron');
+
+electron.app.on('ready', () => {
+
+  let mainWindow = new electron.BrowserWindow({ width: 600, height: 800 });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+});
